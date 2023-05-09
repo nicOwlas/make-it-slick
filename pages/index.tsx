02e2471937acdb2dcc9b1ec71138949587cb935f
Nicolas Draber <nicolas.draber@gmail.com>
2023-05-09 17:03:39 +0200

    Rephrase title and description

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,13 +26,11 @@ export default function Home() {
       <Box textAlign="center" p={4}>
         <Stack spacing={6}>
           <Heading as="h1" size="2xl" fontWeight={"semibold"}>
-            We Provide You With<br></br>
-            The Right Tools For The Right Job
+            Automate Your Business Processes
           </Heading>
           <Text fontSize="lg=" fontWeight={"medium"}>
-            Modern tools like Notion, Airtable, chatGPT, Zapier etc. allow to
-            boost your team productivity and focus your time where it matters
-            most.
+            We empower businesses to increase productivity by automating
+            repetitive tasks and processes using cutting-edge SaaS tools.
           </Text>
         </Stack>
       </Box>
