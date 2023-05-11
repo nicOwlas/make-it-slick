d46cf4f7fd8d78b701d0ed9b90af2ed7dec39cf9
Nicolas Draber <nicolas.draber@gmail.com>
2023-05-11 22:32:15 +0200

    Adding Google Tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,22 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-1BQT3B3ZB1"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1BQT3B3ZB1');
+            `,
+            }}
+          ></script>
+        </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
