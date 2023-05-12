16d6e38b9a51a07ecb0466fdbc5addb628937f94
Nicolas Draber <nicolas.draber@gmail.com>
2023-05-12 10:37:10 +0200

    Use next/script to init google-tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,6 +2,7 @@
 
 import { ColorModeScript } from "@chakra-ui/react";
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 import theme from "../theme";
 
 class MyDocument extends Document {
@@ -9,20 +10,18 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <script
-            async
+          <Script
             src="https://www.googletagmanager.com/gtag/js?id=G-1BQT3B3ZB1"
-          ></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+            strategy="afterInteractive"
+          />
+          <Script id="gtag-init" strategy="afterInteractive">
+            {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-1BQT3B3ZB1');
-            `,
-            }}
-          ></script>
+            `}
+          </Script>
         </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
