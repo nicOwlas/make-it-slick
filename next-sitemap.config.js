f07b61a82b5dd8bbb44be878fe46cbe2567f5f3a
Nicolas Draber <nicolas.draber@gmail.com>
2023-05-12 10:36:33 +0200

    Adding sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: "https://www.makeitslick.com",
+  generateRobotsTxt: true,
+};
--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+//next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
