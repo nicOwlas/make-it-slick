0171af34b1e9bf215e2c9d169c0c08247faad73d
Nicolas Draber <nicolas.draber@gmail.com>
2023-05-15 12:58:26 +0200

    Fix bg color issue on sign-up page

--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -1,13 +1,14 @@
+import { Box } from "@chakra-ui/react";
 import { Widget } from "@typeform/embed-react";
 
 export default function SignUp() {
   return (
-    <div style={{ width: "100%", height: "100vh" }}>
+    <Box style={{ width: "100%", height: "100vh" }}>
       <Widget
         id="MnFcFpfA?typeform-welcome=0"
         style={{ height: "100%", width: "100%" }}
         opacity={0}
       />
-    </div>
+    </Box>
   );
 }
